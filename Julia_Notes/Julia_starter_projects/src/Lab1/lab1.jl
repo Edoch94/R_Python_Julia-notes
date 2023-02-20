8ccb455875a53c97cb75ed5abbef99990278434d
edoch <edo_chio@hotmail.it>
2023-02-21 00:26:04 +0100

    lab 1 puntata 3

--- Julia_Notes/Julia_starter_projects/src/Lab1/lab1.jl
+++ Julia_Notes/Julia_starter_projects/src/Lab1/lab1.jl
@@ -7,34 +7,49 @@ using InteractiveUtils
 # ╔═╡ d4e35bda-af13-11ed-075a-d13f3e7004ee
 using CSV
 
+# ╔═╡ 2d320ec9-0636-4da5-818e-6ed77dfb15e9
+using DataFrames
+
+# ╔═╡ c76e706f-fd37-49ce-b477-9488e0fb9551
+using Chain
+
+# ╔═╡ 08a983ff-9455-4cb5-8844-1048998108f5
+using Statistics
+
+# ╔═╡ 103ab93b-3ec0-40d8-b9bc-0bed0821152d
+md"
+First task
+"
+
 # ╔═╡ 93f36c12-b0c3-433b-9d58-f38cb628ab91
 readdir(joinpath(pwd(),"data"))
 
 # ╔═╡ 7c3715d9-d26c-4087-a330-cbbdb80bed51
 iris_file = CSV.File(joinpath(pwd(),"data/iris.csv"),header=["sepal_length","sepal_width","petal_length","petal_width","species"])
 
-# ╔═╡ c2db3217-e927-4fbe-adde-6e5e3f407e4c
-begin
-	row_array = Any[]
-	table_array = Any[]
-end
-
 # ╔═╡ ab5bb420-65ee-4582-bc3b-2299351b5839
-for (row) in iris_file
-	row_array = Any[]
-	
-	for (element) in row
-		push!(row_array, element)
+begin
+	table_array = Any[]
+	for row in iris_file
+		row_array = Any[]
 		
+		for (element) in row
+			push!(row_array, element)
+		end
+		push!(table_array,row_array)
 	end
-	push!(table_array,row_array)
 end
 
 # ╔═╡ 7217e5a3-5ff8-428f-97c6-dc270b7ffee7
 println(table_array)
 
+# ╔═╡ a64aba1d-1876-4bf3-b4fb-3534e34dbf06
+md"
+Second task
+"
+
 # ╔═╡ b22da206-8945-40b8-a31d-ca766ea72942
-function mean(arr)
+function mean_custom(arr)
 	sum_array = 0
 	length_array = 0
 	for (i) in arr
@@ -45,26 +60,74 @@ function mean(arr)
 end
 
 # ╔═╡ e7b7ff6d-fc5c-47ea-9c9d-ce3093c1b1bf
-function std(arr)
+function std_custom(arr)
 	mean_array = mean(arr)
-	errors = []
+	quad_diffs = [i - mean_array for i in arr].^2
+	sum_quad_diffs = 0
+	length_array = 0
+	for i in quad_diffs
+		sum_quad_diffs += i
+		length_array += 1
+	end
+	(sum_quad_diffs / length_array) ^ (1/2)
 end
 
 # ╔═╡ 5ec00c56-15b4-43a0-90a4-7d4ed3776ed8
 for (column) in 1:4
-	println(mean([x[column] for (x) in table_array]))
+	mean_value, std_value = mean_custom([x[column] for (x) in table_array]), std_custom([x[column] for (x) in table_array])
+	println("$mean_value, $std_value")
 end
 
-# ╔═╡ bf41efa0-80f9-4117-b934-4a4e1ba8c52f
+# ╔═╡ d63fcd12-356a-4d5e-a953-59c9072e1730
+md"
+Third task
+"
+
+# ╔═╡ e5ac3983-a9c1-45f6-b974-8f4ed24d06a7
+function metrics(arr)
+	mean_values = Vector{Float64}()
+	std_values = Vector{Float64}()
+	for (column) in 1:4
+		append!(mean_values, mean_custom([x[column] for x in arr]))
+		append!(std_values, std_custom([x[column] for x in arr]))
+	end
+	return (mean_values, std_values)
+end
+
+# ╔═╡ a5fc8f11-dc4c-437a-b2c4-178d08280438
+metrics(table_array)
+
+# ╔═╡ 3fd62827-8df5-4050-959b-9644e5a06360
+for species in Set([i[5] for i in table_array])
+	println("$species")
+	mean_value, std_value = metrics([i for i in table_array if i[5]==species])
+	println("means: $mean_value,\nstds: $std_value\n")
+end
+
+# ╔═╡ 69893655-1915-4e4d-b533-d60a53c9d50d
+df = CSV.read(joinpath(pwd(),"data/iris.csv"), DataFrame; header=["sepal_length","sepal_width","petal_length","petal_width","species"])
+
+# ╔═╡ ca7d1b25-2105-44f5-812c-2c2872e9ebc7
+@chain df begin
+	groupby(:species)
+	combine(:sepal_length => mean)
+end
+
+# ╔═╡ 68ccddec-0476-4c94-8fb1-197b3d901d25
 
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 CSV = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
+Chain = "8be319e6-bccf-4806-a6f7-6fae938471bc"
+DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+Statistics = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 
 [compat]
 CSV = "~0.10.9"
+Chain = "~0.5.0"
+DataFrames = "~1.5.0"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -73,7 +136,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.5"
 manifest_format = "2.0"
-project_hash = "fa2a62b9d769ed031d8341a3e1528564bcba5b63"
+project_hash = "efbd1deca40461dc092148ef7e2e011252b70f0c"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -86,6 +149,11 @@ deps = ["CodecZlib", "Dates", "FilePathsBase", "InlineStrings", "Mmap", "Parsers
 git-tree-sha1 = "c700cce799b51c9045473de751e9319bdd1c6e94"
 uuid = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
 version = "0.10.9"
+
+[[deps.Chain]]
+git-tree-sha1 = "8c4920235f6c561e401dfe569beb8b924adad003"
+uuid = "8be319e6-bccf-4806-a6f7-6fae938471bc"
+version = "0.5.0"
 
 [[deps.CodecZlib]]
 deps = ["TranscodingStreams", "Zlib_jll"]
@@ -104,10 +172,27 @@ deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
 version = "1.0.1+0"
 
+[[deps.Crayons]]
+git-tree-sha1 = "249fe38abf76d48563e2f4556bebd215aa317e15"
+uuid = "a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f"
+version = "4.1.1"
+
 [[deps.DataAPI]]
 git-tree-sha1 = "e8119c1a33d267e16108be441a287a6981ba1630"
 uuid = "9a962f9c-6df0-11e9-0e5d-c546b8b5ee8a"
 version = "1.14.0"
+
+[[deps.DataFrames]]
+deps = ["Compat", "DataAPI", "Future", "InlineStrings", "InvertedIndices", "IteratorInterfaceExtensions", "LinearAlgebra", "Markdown", "Missings", "PooledArrays", "PrettyTables", "Printf", "REPL", "Random", "Reexport", "SentinelArrays", "SnoopPrecompile", "SortingAlgorithms", "Statistics", "TableTraits", "Tables", "Unicode"]
+git-tree-sha1 = "aa51303df86f8626a962fccb878430cdb0a97eee"
+uuid = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+version = "1.5.0"
+
+[[deps.DataStructures]]
+deps = ["Compat", "InteractiveUtils", "OrderedCollections"]
+git-tree-sha1 = "d1fff3a548102f48987a52a2e0d114fa97d730f0"
+uuid = "864edb3b-99cc-5e75-8d2d-829cb0a9cfe8"
+version = "0.18.13"
 
 [[deps.DataValueInterfaces]]
 git-tree-sha1 = "bfc1187b79289637fa0ef6d4436ebdfe6905cbd6"
@@ -124,6 +209,12 @@ git-tree-sha1 = "e27c4ebe80e8699540f2d6c805cc12203b614f12"
 uuid = "48062228-2e41-5def-b9a4-89aafe57970f"
 version = "0.9.20"
 
+[[deps.Formatting]]
+deps = ["Printf"]
+git-tree-sha1 = "8339d61043228fdd3eb658d86c926cb282ae72a8"
+uuid = "59287772-0a20-5a39-b81b-1366585eb4c0"
+version = "0.4.2"
+
 [[deps.Future]]
 deps = ["Random"]
 uuid = "9fa8497b-333b-5362-9e8d-4d0656e87820"
@@ -138,10 +229,20 @@ version = "1.4.0"
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
 
+[[deps.InvertedIndices]]
+git-tree-sha1 = "82aec7a3dd64f4d9584659dc0b62ef7db2ef3e19"
+uuid = "41ab1584-1d38-5bbf-9106-f11c6c58b48f"
+version = "1.2.0"
+
 [[deps.IteratorInterfaceExtensions]]
 git-tree-sha1 = "a3f24677c21f5bbe9d2a714f95dcd58337fb2856"
 uuid = "82899510-4779-5014-852e-03e436cf321d"
 version = "1.0.0"
+
+[[deps.LaTeXStrings]]
+git-tree-sha1 = "f2355693d6778a178ade15952b7ac47a4ff97996"
+uuid = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
+version = "1.3.0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -156,6 +257,12 @@ uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
 [[deps.Markdown]]
 deps = ["Base64"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.Missings]]
+deps = ["DataAPI"]
+git-tree-sha1 = "f66bdc5de519e8f8ae43bdc598782d35a25b1272"
+uuid = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
+version = "1.1.0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
@@ -188,13 +295,28 @@ git-tree-sha1 = "47e5f437cc0e7ef2ce8406ce1e7e24d44915f88d"
 uuid = "21216c6a-2e73-6563-6e65-726566657250"
 version = "1.3.0"
 
+[[deps.PrettyTables]]
+deps = ["Crayons", "Formatting", "LaTeXStrings", "Markdown", "Reexport", "StringManipulation", "Tables"]
+git-tree-sha1 = "96f6db03ab535bdb901300f88335257b0018689d"
+uuid = "08abe8d2-0d0c-5749-adfa-8a2ac140af0d"
+version = "2.2.2"
+
 [[deps.Printf]]
 deps = ["Unicode"]
 uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
 
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
 [[deps.Random]]
 deps = ["SHA", "Serialization"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
@@ -214,6 +336,28 @@ deps = ["Preferences"]
 git-tree-sha1 = "e760a70afdcd461cf01a575947738d359234665c"
 uuid = "66db9d55-30c0-4569-8b51-7e840670fc0c"
 version = "1.0.3"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SortingAlgorithms]]
+deps = ["DataStructures"]
+git-tree-sha1 = "a4ada03f999bd01b3a25dcaa30b2d929fe537e00"
+uuid = "a2af1166-a08f-5f64-846c-94a0d3cef48c"
+version = "1.1.0"
+
+[[deps.SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[deps.StringManipulation]]
+git-tree-sha1 = "46da2434b41f41ac3594ee9816ce5541c6096123"
+uuid = "892a3eda-7b42-436c-8928-eab12a02cf0e"
+version = "0.3.0"
 
 [[deps.TOML]]
 deps = ["Dates"]
@@ -272,15 +416,25 @@ version = "5.1.1+0"
 """
 
 # ╔═╡ Cell order:
+# ╠═103ab93b-3ec0-40d8-b9bc-0bed0821152d
 # ╠═d4e35bda-af13-11ed-075a-d13f3e7004ee
 # ╠═93f36c12-b0c3-433b-9d58-f38cb628ab91
 # ╠═7c3715d9-d26c-4087-a330-cbbdb80bed51
-# ╠═c2db3217-e927-4fbe-adde-6e5e3f407e4c
 # ╠═ab5bb420-65ee-4582-bc3b-2299351b5839
 # ╠═7217e5a3-5ff8-428f-97c6-dc270b7ffee7
+# ╠═a64aba1d-1876-4bf3-b4fb-3534e34dbf06
 # ╠═b22da206-8945-40b8-a31d-ca766ea72942
 # ╠═e7b7ff6d-fc5c-47ea-9c9d-ce3093c1b1bf
 # ╠═5ec00c56-15b4-43a0-90a4-7d4ed3776ed8
-# ╠═bf41efa0-80f9-4117-b934-4a4e1ba8c52f
+# ╠═d63fcd12-356a-4d5e-a953-59c9072e1730
+# ╠═e5ac3983-a9c1-45f6-b974-8f4ed24d06a7
+# ╠═a5fc8f11-dc4c-437a-b2c4-178d08280438
+# ╠═3fd62827-8df5-4050-959b-9644e5a06360
+# ╠═2d320ec9-0636-4da5-818e-6ed77dfb15e9
+# ╠═c76e706f-fd37-49ce-b477-9488e0fb9551
+# ╠═08a983ff-9455-4cb5-8844-1048998108f5
+# ╠═69893655-1915-4e4d-b533-d60a53c9d50d
+# ╠═ca7d1b25-2105-44f5-812c-2c2872e9ebc7
+# ╠═68ccddec-0476-4c94-8fb1-197b3d901d25
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
